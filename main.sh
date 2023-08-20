8677e939e8387c55071baa00181f151525c5016f
Aaron Hardy <ashtarcommunications@gmail.com>
2023-08-19 21:32:38 -0600

    Some defaults

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
-export GITHUB_BRANCH=${GITHUB_REF##*heads/}
+export SLACK_CHANNEL=${SLACK_CHANNEL:-"#tech-updates"}
 export SLACK_USERNAME=${SLACK_USERNAME:-"GitHub Actions"}
+export SLACK_ICON=${SLACK_ICON:-":x:"}
 export COMMIT_MESSAGE=$(cat "$GITHUB_EVENT_PATH" | jq -r '.commits[-1].message')
 
 slack-notify "$@"
